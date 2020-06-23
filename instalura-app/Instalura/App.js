aaf80a4c2ffbd76c672170ae1bf14cc2126c0d22
Robson Lima <rsl.robson@gmail.com>
2020-06-23 18:28:14 -0300

    Integrando com Servidor e usando estados no React Native

--- instalura-app/Instalura/App.js
+++ instalura-app/Instalura/App.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 
 import {
   ScrollView,
@@ -8,21 +8,32 @@ import {
 import { Cabecalho } from './src/Components/Cabecalho';
 import {Foto} from './src/Components/Foto'
 
-const informacoes = [
-  {id:1, usuario:"Robson"},
-  {id:2, usuario:"Julia"},
-  {id:3, usuario:"Ricardo"},
-]
-
 const App = () => {
+  //variavel do meu estado: fotos
+  //funcao a ser chamada ao alterar o estado: setFotos
+  const [fotos, setFotos] = useState([])
+  //useEffect não premite retorno, por isso declaramos uma função dentro de outra para fazer o fetch
+  useEffect(()=>{
+    const lerFotos = async() =>{
+      //usar o ip da máquina no fetch ao invés de localhost
+      const fotosHTTP  = await fetch("http://localhost:3030/feed");
+      //converte resposta em json
+      const fotosJson = await fotosHTTP.json();
+      //captura as fotos do json
+      setFotos(fotosJson);
+    }
+
+    lerFotos();
+  },[])
+
   return (
     <ScrollView>
       <FlatList
-        data={informacoes}
+        data={fotos}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) =>
           <Fragment>
-            <Cabecalho nomeUsuario={item.usuario} />
+            <Cabecalho nomeUsuario={item.userName} />
             <Foto/> 
           </Fragment>    
         }
