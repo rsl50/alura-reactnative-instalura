4aa3270a5e4b81e9cc9e966f9170c0e55ec453e9
Robson Lima <rsl.robson@gmail.com>
2020-06-23 20:33:47 -0300

    Adicionando entrada e exibição de comentários com usuário e comentário

--- instalura-app/Instalura/src/Components/Comentarios/Comentarios.js
+++ instalura-app/Instalura/src/Components/Comentarios/Comentarios.js
@@ -11,22 +11,53 @@ import {
 import estilo from '../Comentarios/estilo';
 
 const Comentarios = ( {comentarios} ) => {
-    
+    const [estadoComentarios, setComentarios] = useState(comentarios)
+
+    const adicionarComentario = () => {
+        console.warn(conteudoCampoInput);
+        
+        //Limpa campo input
+        campoInput.clear();
+        
+        //Cria novo comentário
+        const novoComentario = {
+            date: Date.now(),
+            text: conteudoCampoInput,
+            userName: "Bugman"
+        }
+
+        // Adiciona novo comentário ao comentários existentes
+        setComentarios([...estadoComentarios, novoComentario]);
+    }
+
+
+    // armazena texto do comentario
+    let conteudoCampoInput = "";
+    let campoInput;
+
     return (
         <Fragment>
             <FlatList
-                data={comentarios}
+                data={estadoComentarios}
                 keyExtractor={(item, index) => index.toString()}
                 renderItem={({ item }) =>
-                <Text>{item.text}</Text>}
+                    <View style={estilo.naMesmaLinha}>
+                        <Text>{item.userName} </Text>
+                        <Text> {item.text}</Text>
+                    </View>
+                }
             />
             <View style={estilo.naMesmaLinha}>
                 <TextInput 
+                    // envia a referencia do componente para campoInput
+                    ref={textInput => campoInput = textInput}
+                    // o callback de onChangeText é armazenado na variavel texto
+                    onChangeText={texto => conteudoCampoInput = texto}
                     placeholder={"Deixe seu comentário..."}
                     //faz o textinput ocupar toda a largura da tela
                     style={{flex:1}}
                 />
-                <TouchableOpacity>
+                <TouchableOpacity onPress={adicionarComentario}>
                     <Image 
                         source={require("../../../res/img/send.png")}
                         style={estilo.imgSend}/>
